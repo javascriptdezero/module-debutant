5a67920ee60d71b94584eee9ffc67fa0f6c0091c
JavaScript de Zéro <jeremy@javascriptdezero.com>
2018-12-18 17:31:13 +0100

    Ajout du cours sur les nombres

---
+++ cours/06 - Les nombres/cours.js
@@ -0,0 +1,63 @@
+
+/* Les nombres */
+
+// Rappel : un nombre = des chiffres
+// 423
+
+// Pas besoin de délimiteur pour les nombres
+// console.log(42);
+
+// Opérations de base, +, -, *, /
+// console.log(33 + 10);
+// console.log(48 - 4);
+// console.log(5 * 9);
+// console.log(92 / 2);
+
+// Expression, opérandes et évaluation
+// console.log(40 + 7);
+// console.log(47);
+
+// Qu'est-ce qu'un nombre entier ? Un nombre à virgule ?
+// console.log(10 / 4);
+
+// Délimiteur décimal point et pas virgule...
+// console.log(2.5);
+
+// Grouper avec les parenthèses
+// console.log((4 + 8) / 2);
+
+// Nombres négatifs (température)
+// console.log(-7);
+// console.log(4 - 12);
+// console.log(4 + -6);
+
+/* Composition avec les chaînes de caractères */
+
+// Opérateur '+' : addition ou concaténation ?
+// console.log(40 + 2);
+// console.log(40 + "bonjour");
+// console.log("40" + "bonjour");
+// console.log("40bonjour");
+
+// Les chiffres sont des caractères
+// console.log("40" + "2");
+
+// Opérateur '-' : soustraction
+// console.log(40 - 2);
+// console.log(40 - "bonjour");
+// console.log(40 - NaN);
+// console.log(NaN);
+// NaN : 🇬🇧 Not a Number (🇫🇷 Pas un Nombre)
+// console.log("40" - "2");
+// console.log(40 - 2);
+
+// Opérateur '*' : multiplication
+// console.log(40 * 2);
+// console.log(40 * "2");
+// console.log(40 * "bonjour");
+
+// Opérateur '/' : division
+// console.log(40 / 2);
+// console.log(40 / "2");
+// console.log(40 / "bonjour");
+
