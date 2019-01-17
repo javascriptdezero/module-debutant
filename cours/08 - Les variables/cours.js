a59c2dab92d937d260b2acc24ef7d3852fa798fe
JavaScript de Zéro <jeremy@javascriptdezero.com>
2019-01-17 10:13:18 +0100

    Modification du contenu du cours sur les variables

--- cours/08 - Les variables/cours.js
+++ cours/08 - Les variables/cours.js
@@ -3,7 +3,7 @@
 
 // Déclarer une variable
 // Mot-clés : let (ES6+), var (< ES6)
-// let maVariable;
+let maVariable;
 // var maVariable;
 
 // 'let' vient des mathématiques
@@ -14,15 +14,13 @@
 
 // Premier caractère du nom : '$' OU '_' OU lettre
 // Reste : '$' OU '_' OU lettre OU chiffre
-let distanceEnMètres;
-let élèveStudieux;
-let $vaRiaBlE;
-let _autrevaleur42;
-let $__abc$_over9000;
-let _abc_$1337$;
-let привет;
-let صباحالخير;
-let 你好;
+// let élèveStudieux;
+// let _autrevaleur42;
+// let $__abc$_over9000;
+// let _abc_$1337$;
+// let привет;
+// let صباحالخير;
+// let 你好;
 
 // Exemple de mauvais nom
 // let 100patates;
@@ -30,20 +28,20 @@ let 你好;
 // let laPonctuation,C'estPasAutoriséNonPlus!;
 
 // Nom !== mot-clés réservés : détecter un problème
-let break;
-let case;
-let catch;
-let continue;
-let export;
-let import;
-let interface;
-let public;
-let super;
+// let break;
+// let case;
+// let catch;
+// let continue;
+// let export;
+// let import;
+// let interface;
+// let public;
+// let super;
 
 /* Convention et bonnes pratiques */
 
 // Faciliter l'écriture de code dans le monde entier
-// On n'utilise que [a-zA-Z0-9] dans les noms
+// On n'utilise que [a-zA-Z0-9] dans les noms de variables
 // let élèveStudieux
 // let caseàcocher
 
@@ -68,16 +66,34 @@ let super;
 // console.log(maVariable);
 
 // Les valeurs undefined et null
-// undefined
-// null
+// console.log(undefined)
+// console.log(null)
 
-/* Affectation de valeur */
-let maVariable;
-console.log(maVariable);
-maVariable = 42;
-console.log(maVariable);
+/* Affectation de valeurs */
+
+// let maVariable;
+// console.log(maVariable);
+// maVariable = "bonjour";
+// console.log(maVariable);
 
 // Affectation à la déclaration
-let maVariable = 42;
-console.log(maVariable);
+// let maVariable = 42;
+// console.log(maVariable);
 
+// Affectation d'expressions numériques
+// let maVariable = 5 * 4;
+// let resultat = maVariable * 2 + 2;
+// console.log(resultat);
+
+// Affectation de chaînes de caractères
+// let prenom = "Jérémy";
+// let verbe = " enseigne le ";
+// let langage = "JavaScript";
+// let messageFinal = prenom + verbe + langage;
+// console.log(messageFinal);
+
+// Affectation de booléens
+// let vrai = true;
+// let faux = false;
+// let resultat = vrai || faux;
+// console.log("vrai OU faux retourne : " + resultat);
