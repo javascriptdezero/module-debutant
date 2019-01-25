f7c6bb2eafbe2cc225f88cbbf70118ef75de2d6d
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-01-25 14:21:37 +0100

    Ajout des exercices sur les variables

---
+++ cours/08 - Les variables/solutions.js
@@ -0,0 +1,29 @@
+/*
+ * Il existe une infinité de façons de résoudre un même problème.
+ * Ces solutions ne sont que des exemples. Si vous avez utilisé un autre moyen
+ * et que ça fonctionne, votre solution est valide aussi !
+ */
+
+// Chassez le bogue 0
+let nomParc = "Le Royaume Magique";
+console.log("Bienvenue au parc d'attractions " + nomParc);
+
+// Chassez le bogue 1
+let clientsHier = 359;
+let clientsAujourdhui = 374;
+let totalClients = clientsHier + clientsAujourdhui;
+console.log(totalClients);
+
+// Chassez le bogue 2
+// "case" est un mot-clé réservé, vous n'avez pas le droit de l'utiliser comme nom de variable !
+let nombreDeCases = 64;
+let superficieCase = 2;
+let superficie = nombreDeCases * superficieCase;
+console.log("Superficie du plateau : " + superficie + "m2");
+
+// Chassez le bogue 3
+let heures = 13;
+let minutes = 37;
+let separateur = "h";
+let horaire = heures + separateur + minutes;
+console.log("Bienvenue, il est " + horaire);
---
+++ cours/08 - Les variables/validateur/reponses.js
@@ -0,0 +1,19 @@
+/*
+ * N'hésitez pas à jeter un oeil 👁 sur ce code et à me prévenir
+ * si vous rencontrez un bogue !
+ *
+ * Vous pouvez me joindre par email sur jeremy@javascriptdezero.com
+ * ou via Slack : https://javascriptdezero.slack.com
+ *
+ * Merci 👍
+ */
+
+/* eslint-disable no-unused-vars */
+
+/* globals reponsesEnonces */
+const reponsesEnonces = [
+  "Bienvenue au parc d'attractions Le Royaume Magique",
+  733,
+  "Superficie du plateau : 128m2",
+  "Bienvenue, il est 13h37",
+];
