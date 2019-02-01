a4a9e0710be6f32848e6d4b6b3c253c884a6e62f
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-02-01 09:51:25 +0100

    Pas de cours ni d'exercices pour le débogueur, juste les liens vers les docs

---
+++ cours/09 - Le débogueur/cours.js
@@ -0,0 +1,17 @@
+
+/* Le débogueur */
+
+/*
+
+Il n'y a pas de cours ni d'exercices pour le débogueur.
+
+Entraînez-vous à l'utiliser sur des petits programmes que vous aurez rédigé
+ou récupéré sur Internet.
+
+Pensez à lire la documention de chaque débogueur !
+Doc (en Anglais) débogueur Chrome : https://developers.google.com/web/tools/chrome-devtools/javascript
+Doc (en Anglais) débogueur VSCode : https://code.visualstudio.com/Docs/editor/debugging
+
+Bon débogage :) !
+
+*/
