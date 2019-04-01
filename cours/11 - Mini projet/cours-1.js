39b81f30a4697539e055740a80811d5b3962e595
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-04-01 16:12:38 +0200

    Ajout des étapes dans les commentaires

--- cours/11 - Mini projet/cours-1.js
+++ cours/11 - Mini projet/cours-1.js
@@ -1,7 +1,7 @@
 /* Mini projet : valider un rendez-vous */
 // Retrouvez chaque version du projet dans cours-1.js, cours-2.js etc.
 
-// Version 1 : pour une durée de RDV de 15 minutes
+// Exemple 1 : pour une durée de RDV de 15 minutes
 
 // Début du RDV : 15h40
 let debutRdvHeures = 15;
@@ -14,15 +14,16 @@ let finJourneeMinutes = 30;
 // Durée du RDV : 15 minutes
 let duree = 15;
 
-// Calcul de la fin du RDV
+// ETAPE 1 : Calcul de la fin du RDV
 let finRdvHeures = debutRdvHeures;
 let finRdvMinutes = debutRdvMinutes + duree;
 
-// Est-ce que la fin du RDV est avant la fin de la journée ?
+// ETAPE 2 : Est-ce que la fin du RDV est avant la fin de la journée ?
 if (finRdvHeures < finJourneeHeures) {
   console.log("RDV OK");
 } else {
   console.log("RDV impossible");
 }
 
+// ETAPE 3 : Affichage de l'heure de fin du RDV
 console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
