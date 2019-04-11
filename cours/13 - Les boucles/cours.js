6693371ecc2afba8537093227850bec2ff09f9e8
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-04-11 15:41:15 +0200

    Ajout du cours sur les boucles

--- cours/13 - Les boucles/cours.js
+++ cours/13 - Les boucles/cours.js
@@ -1,16 +1,17 @@
 /* Les boucles */
 
 // La boucle while (tant que)
-// let compteur = 0;
-// while (compteur < 3) {
-//   console.log("compteur : " + compteur);
-//   console.log("Une instruction à exécuter");
-//   compteur = compteur + 1;
-// }
+let compteur = 0;
+while (compteur < 3) {
+  console.log("compteur : " + compteur);
+  console.log("Instruction de la boucle while");
+  compteur = compteur + 1;
+}
 
-// console.log("Ligne pour éviter que le débogueur saute à la fin du fichier...");
+console.log("Ligne pour éviter que le débogueur saute à la fin du fichier...");
 
 // Boucle infinie
+// let compteur = 0;
 // while (true) {
 //   console.log("compteur : " + compteur);
 //   compteur = compteur + 1;
@@ -22,9 +23,30 @@
 // Jeux vidéo, Système embarqué / OS
 
 // Boucle do... while (faire... tant que)
-let compteur = 0;
-do {
-  console.log("compteur : " + compteur);
-  console.log("Une instruction à exécuter");
-  compteur = compteur + 1;
-} while (compteur < 3);
+// let compteur = 0;
+// do {
+//   console.log("compteur : " + compteur);
+//   console.log("Instruction de la boucle do... while");
+//   compteur = compteur + 1;
+// } while (compteur < 3);
+
+// console.log("Ligne pour éviter que le débogueur saute à la fin du fichier...");
+
+// Boucle for
+// for (let compteur = 0; compteur < 3; compteur = compteur + 1) {
+//   console.log("compteur : " + compteur);
+//   console.log("Instruction de la boucle for");
+// }
+
+// console.log("Ligne pour éviter que le débogueur saute à la fin du fichier...");
+
+// On n'est pas obligé d'utiliser des nombres
+// let rireMoqueur = "Mouah";
+// while (rireMoqueur !== "Mouahahahahah") {
+//   rireMoqueur = rireMoqueur + "ah";
+// }
+// console.log(rireMoqueur);
+
+// console.log("Ligne pour éviter que le débogueur saute à la fin du fichier...");
+
+/* Comparaisons visuelles des boucles */
