0bf6a7f3327faee49b4d651fecceb6ec9c8a664b
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-04-17 12:00:10 +0200

    Ajout du brouillon de la lecon mini projet 3

---
+++ cours/14 - Mini projet 3/cours.js
@@ -0,0 +1,40 @@
+/* Mini projet : valider un rendez-vous */
+// Retrouvez chaque version du projet dans cours-1.js, cours-2.js etc.
+
+// Exemple 3 : pour une durée de RDV de 90 minutes
+
+// Début du RDV : 15h40
+let debutRdvHeures = 15;
+let debutRdvMinutes = 40;
+
+// Fin de journée : 16h30
+let finJourneeHeures = 16;
+let finJourneeMinutes = 30;
+
+// Durée du RDV : 90 minutes
+let duree = 90;
+
+// ETAPE 1 : Calcul de la fin du RDV
+let finRdvHeures = debutRdvHeures;
+let finRdvMinutes = debutRdvMinutes + duree;
+
+// ETAPE 1.1 : Gestion du cas particulier où les minutes sont >= 60
+if (finRdvMinutes >= 60) {
+  finRdvHeures = finRdvHeures + 1;
+  finRdvMinutes = finRdvMinutes - 60;
+}
+
+// ETAPE 2 : Est-ce que la fin du RDV est avant la fin de la journée ?
+if (finRdvHeures < finJourneeHeures
+  || (finRdvHeures === finJourneeHeures && finRdvMinutes < finJourneeMinutes)) {
+  console.log("RDV OK");
+} else {
+  console.log("RDV impossible");
+}
+
+// ETAPE 3 : Affichage de l'heure de fin du RDV
+if (finRdvMinutes < 10) {
+  console.log("Le RDV se terminera à " + finRdvHeures + "h0" + finRdvMinutes);
+} else {
+  console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
+}
