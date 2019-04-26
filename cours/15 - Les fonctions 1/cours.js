707b4b2acacc258bc5f765254e887b4880de8905
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-04-26 10:43:20 +0200

    Brouillon du cours 1 sur les fonctions

---
+++ cours/15 - Les fonctions 1/cours.js
@@ -0,0 +1,27 @@
+/* Les fonctions */
+
+// Déclarer une fonction
+function maFonction() {
+  console.log("Instruction 1");
+  console.log("Instruction 2");
+}
+
+// Appel de la fonction
+maFonction();
+
+
+/* Utiliser des paramètres */
+
+// Déclaration avec paramètres
+// function maFonction(prenom) {
+//   console.log("Bienvenue " + prenom + " !");
+// }
+
+// Valeur codée en dur
+// maFonction("Tom");
+
+// Valeur passée via une variable
+// let monPrenom = "Jérémy";
+// maFonction(monPrenom);
+
+// console.log("Fin du code");
