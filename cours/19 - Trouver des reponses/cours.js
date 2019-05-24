3b5426ab58b6c9f0de02a29e1d8a0900fa3f9397
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-05-24 10:03:52 +0200

    Brouillon cours dernière vidéo du module débutant

---
+++ cours/19 - Trouver des reponses/cours.js
@@ -0,0 +1,21 @@
+/* Trouver des réponses sur Internet */
+
+// Résoudre un problème de syntaxe
+// function souhaiterLaBienvenue(prenom) {
+//   console.log("Bienvenue ' + prenom + " dans cette formation !");
+// }
+
+let maVariable = "bonjour";
+console.log(mavariable);
+
+// Résoudre un problème fonctionnel
+// function estCeZero(chiffre) {
+//   console.log("Est-ce que le chiffre " + chiffre + " est zéro ?");
+//   if (chiffre = 0) {
+//     console.log("Bien sûr que oui, c'est zéro !");
+//   } else {
+//     console.log("Non, ce n'est pas zéro 😭");
+//   }
+// }
+
+// estCeZero(0);
