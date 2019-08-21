65b99757b4d8f0936d1366ed09eecb338bebde6e
Javascript de Zéro <jeremy@javascriptdezero.com>
2019-08-21 14:52:51 +0200

    Correction d'erreurs dans solutions.js du cours 07 sur les booléens, exercice 2 et 3. Il faut utiliser >= et pas > uniquement pour coller à l'énoncé.

--- cours/07 - Les booleens/solutions.js
+++ cours/07 - Les booleens/solutions.js
@@ -11,10 +11,10 @@ console.log(65 < 70 && 4 < 3);
 console.log(140 > 150 || 16 >= 14);
 
 // Exercice 2
-console.log((15 > 16 || 170 > 150) && 79 < 80);
+console.log((15 > 16 || 170 >= 150) && 79 < 80);
 
 // Exercice 3
-console.log(10 > 12 || 110 > 120 || (58 > 40 && 58 < 60));
+console.log(10 > 12 || 110 >= 120 || (58 > 40 && 58 < 60));
 
 // Chassez le bogue 0
 console.log((150 >= 120 || 16 > 15) && 95 < 90);
