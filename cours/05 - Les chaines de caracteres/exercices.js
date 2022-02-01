878a258d694b29b507875da9f42fd718db6051f8
Yannick Biheul <yannickbiheul@outlook.fr>
2022-02-01 09:52:33 +0100

    05- Les chaines de caractères

--- cours/05 - Les chaines de caracteres/exercices.js
+++ cours/05 - Les chaines de caracteres/exercices.js
@@ -9,23 +9,36 @@
  */
 
 // Exercice 0
-
+console.log("JavaScript est facile");
 // Exercice 1
-
+console.log("JavaScript c'est facile");
 // Exercice 2
-
+console.log(
+    "C'est Lao Tseu qui a dit : \"L'échec est le fondement de la réussite\""
+);
 // Exercice 3
-
+console.log("Afficher un \\ n'est pas si évident");
 // Exercice 4
-
+console.log("Ceci est la première ligne\nCeci est la deuxième ligne");
 // Exercice 5
-
+console.log(
+    `Un jour le JavaScript n'aura plus de secrets pour moi
+Lire du code ne sera plus une gageure mais une joie
+Mes doigts seront habitués à tous ces symboles particuliers
+Et ma créativité de développeur pourra enfin s'exprimer`
+);
 // Chassez le bogue 0
 // Retirez les commentaires devant le code de Tom pour commencer
-// console.log("Bonjour' + 'Bob' + ', aujourd'hui nous faisons une promo 'la fidélité paye' !");
+console.log(
+    "Bonjour " +
+        "Bob" +
+        ', aujourd\'hui nous faisons une promo "la fidélité paye" !'
+);
 
 // Chassez le bogue 1
 // Retirez les commentaires devant le code de Tom pour commencer (les 3 lignes ci-dessous)
-// console.log('        Bonjour' + 'Bob' + ",  \
-// \
-// Aujourd\'hui nous faisons une promo \"la fidélité paye\" !");
+console.log(
+    "\t\tBonjour " +
+        "Bob" +
+        ',\n\nAujourd\'hui nous faisons une promo "la fidélité paye" !'
+);
